2eadc84f87a0c9b8dc0c49e26a0159fd6ead6cbc
audstudios <billy.e.riley@gmail.com>
2026-01-28 20:35:46 -0500

    asacii update

--- components/BlogScreen/AsciiArt.tsx
+++ components/BlogScreen/AsciiArt.tsx
@@ -20,7 +20,6 @@ export default function AsciiArt({
       <div 
         className="ascii-art"
         style={{ 
-          fontSize: '13px',
           lineHeight: '1',
           color,
           whiteSpace: 'pre',
@@ -41,7 +40,6 @@ export default function AsciiArt({
       className="ascii-art"
       id="ascii-art-container"
       style={{ 
-        fontSize: '7px',
         lineHeight: '1',
         color,
         display: 'flex',
