295cffef5f292e113ab1bd95c393781b2155500d
audstudios <billy.e.riley@gmail.com>
2026-02-08 21:13:55 -0500

    Added link

--- components/BlogScreen/AsciiArt.tsx
+++ components/BlogScreen/AsciiArt.tsx
@@ -17,34 +17,58 @@ export default function AsciiArt({
   if (!animated) {
     // Secret blog: render instantly
     return (
-      <div 
-        className="ascii-art"
+      <a 
+        href="https://www.basementisaband.com/" 
+        target="_blank" 
+        rel="noopener noreferrer"
         style={{ 
-          lineHeight: '1',
-          color,
-          whiteSpace: 'pre',
-          display: 'flex',
-          justifyContent: 'center'
+          textDecoration: 'none',
+          display: 'block',
+          cursor: 'pointer'
         }}
       >
-        <div style={{ textAlign: 'left' }}>
-          {art}
+        <div 
+          className="ascii-art"
+          style={{ 
+            lineHeight: '1',
+            color,
+            whiteSpace: 'pre',
+            display: 'flex',
+            justifyContent: 'center',
+            cursor: 'pointer'
+          }}
+        >
+          <div style={{ textAlign: 'left' }}>
+            {art}
+          </div>
         </div>
-      </div>
+      </a>
     )
   }
 
   // Normal blog: will be animated via parent
   return (
-    <div 
-      className="ascii-art"
-      id="ascii-art-container"
+    <a 
+      href="https://www.basementisaband.com/" 
+      target="_blank" 
+      rel="noopener noreferrer"
       style={{ 
-        lineHeight: '1',
-        color,
-        display: 'flex',
-        justifyContent: 'center'
+        textDecoration: 'none',
+        display: 'block',
+        cursor: 'pointer'
       }}
-    />
+    >
+      <div 
+        className="ascii-art"
+        id="ascii-art-container"
+        style={{ 
+          lineHeight: '1',
+          color,
+          display: 'flex',
+          justifyContent: 'center',
+          cursor: 'pointer'
+        }}
+      />
+    </a>
   )
 }
