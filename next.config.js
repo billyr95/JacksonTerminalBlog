5c345eafcc6c9f5b55fa07eb68c6e0596a0fdcae
audstudios <billy.e.riley@gmail.com>
2026-03-03 09:38:51 -0500

    Re-Route Url

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     domains: ['picsum.photos'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'basementload.ing',
+          },
+        ],
+        destination: 'https://wired-world.uk/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
